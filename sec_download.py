3efa73285eef1e48a67785835f9ffa72c0f272ce
aravindk100 <portlandphotos@gmail.com>
2016-04-07 00:23:50 -0700

    Added CIK

--- sec_download.py
+++ sec_download.py
@@ -4,6 +4,8 @@
 #from SECEdgar.crawler import SecCrawler
 #seccrawler = SecCrawler()
 #seccrawler.filing_10K('AAPL','0000320193','200010101','10')
+import requests
+import re
 
 def downloadfile(ftpfolder,filename):
     from ftplib import FTP
@@ -26,19 +28,28 @@ def downloadfile(ftpfolder,filename):
 # Write function to find subfolder on the fly
 # Get cik or company name on the fly and match to cik
 def ciklookup(ticker):
-    if ticker == 'intc':
-        cik_internal = '50863'
+
+    URL = 'https://www.sec.gov/cgi-bin/browse-edgar?CIK='+ticker+'&owner=exclude&action=getcompany&Find=Search'
+    page = requests.get(URL)
+    ciks = re.findall('CIK=([0-9]+)',page.text)
+    cik_internal = ciks[0]   # getting all the cik using findall can be  cut short to stop at first if needed
     return cik_internal
  
 
 
-def generate_ftp_folder():
-    folder = '000005086315000072'
+def generate_ftp_folder(): #creating the path to travers sec.gov ftp site using the index
+    folder = '000005086315000072' ## TODO make this generic next
     ftpfolderpath = '/' + cik + '/' + folder 
     return ftpfolderpath
+    
+def stripzero(cikzero): # removes the leading zero from CIK for the prupose of navigation into ftp folder
+    cik_int = re.findall('([1-9][0-9]+)',cikzero)
+    return str(cik_int[0])
 
 ticker = raw_input('Enter the Stock ticker')
-cik = ciklookup(ticker)
+onecik = ciklookup(ticker)
+cik = stripzero(onecik)
+print 'CIK of ticker %s is %s' %(ticker,cik)
 ftpfolder = generate_ftp_folder()
 
 
