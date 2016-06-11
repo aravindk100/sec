ef3a677a2dd66edaf440b9ea421c66297a32258d
aravindk100 <portlandphotos@gmail.com>
2016-06-10 20:49:32 -0700

    Plotter matplotlib can plot by date now...

--- plotter.py
+++ plotter.py
@@ -1,22 +1,35 @@
 import matplotlib.pyplot as plt
 import sqlite3
+import datetime
 
 
 conn = sqlite3.connect('secfinance.sqlite')
 cur = conn.cursor()
 
-valuetype = 1
+valid = []
+valtypname = []
+
+cur.execute('''SELECT * FROM valuetype''')
+for valtyp in cur.fetchall():
+    print valtyp
+    valid.append(valtyp[0])
+    valtypname.append(valtyp[1])
+
+#valuetype = 1
 plotlist = []
 datelist = []
-cur.execute('''SELECT * FROM summary WHERE valuetype_id=?''',(valuetype,))
-for entry in cur.fetchall():
-    print entry
-    datelist.append(entry[2])
-    plotlist.append(entry[3])
-
+for valcount in valid:
+    cur.execute('''SELECT * FROM summary WHERE valuetype_id=?''',(valcount,)) #instead of passing 1 find the valuetype and its primary key at run time
+    for entry in cur.fetchall():
+        dateformat = datetime.datetime.strptime(entry[2], '%Y-%m-%d').date()
+        datelist.append(dateformat)
+        plotlist.append(entry[3])
+    plt.plot_date(datelist,plotlist, fmt='bo', tz=None, xdate=True, ydate=False, ls='solid')
+    #plt.bar(datelist,plotlist,color="red",linestyle='-',linewidth=5)
+    plt.show()
+    plotlist = []
+    datelist = []
+    print valcount
 
 #expand to other rows in income sheet
 
-
-plt.bar(datelist,plotlist,color="red",linestyle='-',linewidth=5)
-plt.show()
