ef0f8d38b20730c6aac4eb606e58ce2552cbe5b4
aravindk100 <portlandphotos@gmail.com>
2016-06-13 23:54:33 -0700

    Plotter draws both graph indivually

--- plotter.py
+++ plotter.py
@@ -10,26 +10,24 @@ valid = []
 valtypname = []
 
 cur.execute('''SELECT * FROM valuetype''')
-for valtyp in cur.fetchall():
+for valtyp in cur.fetchall(): #getting all vallues i.e. netrevenue,netincome etc.
     print valtyp
-    valid.append(valtyp[0])
-    valtypname.append(valtyp[1])
+    valid.append(valtyp[0])  #used for val id
+    valtypname.append(valtyp[1]) # used for name of val type to include in label
 
-#valuetype = 1
 plotlist = []
 datelist = []
-for valcount in valid:
+for index,valcount in enumerate(valid):
     cur.execute('''SELECT * FROM summary WHERE valuetype_id=?''',(valcount,)) #instead of passing 1 find the valuetype and its primary key at run time
     for entry in cur.fetchall():
         dateformat = datetime.datetime.strptime(entry[2], '%Y-%m-%d').date()
         datelist.append(dateformat)
         plotlist.append(entry[3])
     plt.plot_date(datelist,plotlist, fmt='bo', tz=None, xdate=True, ydate=False, ls='solid')
+    plt.title(valtypname[index])
     #plt.bar(datelist,plotlist,color="red",linestyle='-',linewidth=5)
-    plt.show()
     plotlist = []
     datelist = []
-    print valcount
-
+    plt.show()
 #expand to other rows in income sheet
 
